1ba3a11d06427133fcf6a5bf26ecd6f45353ecd5
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-04-16 19:57:07 +0800

    Update shiqi.sh

--- shiqi.sh
+++ shiqi.sh
@@ -2,9 +2,11 @@
 echo "开始安装X-ui"
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 echo "开始下载必要文件"
-wget --no-check-certificate -O /root/config.json https://raw.githubusercontent.com/hkshiqi/x-ui/main/config.json
-chmod +x /root/config.json
-runuser -l root -c 'nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /root/config.json &'
+mkdir /etc/socks5
+chmod +x /etc/socks5
+wget --no-check-certificate -O /etc/socks5/config.json https://raw.githubusercontent.com/hkshiqi/x-ui/main/config.json
+chmod +x /etc/socks5/config.json
+runuser -l root -c 'nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /etc/socks5/config.json &'
 firewall-cmd --zone=public --add-port=20800/tcp --permanent
 firewall-cmd --zone=public --add-port=20800/udp --permanent
 firewall-cmd --reload
