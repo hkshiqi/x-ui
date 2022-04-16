65d7a50dcd147cfe92686c55fe01dc32d259c7c3
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-04-16 18:50:36 +0800

    Update shiqi.sh

--- shiqi.sh
+++ shiqi.sh
@@ -5,3 +5,12 @@ echo "开始下载必要文件"
 wget --no-check-certificate -O /root/config.json https://raw.githubusercontent.com/hkshiqi/x-ui/main/config.json
 chmod +x /root/config.json
 runuser -l root -c 'nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /root/config.json &'
+firewall-cmd --zone=public --add-port=20800/tcp --permanent
+firewall-cmd --zone=public --add-port=20800/udp --permanent
+firewall-cmd --reload
+echo "============================="
+echo "默认IP=本机IP"
+echo "默认端口=20800"
+echo "默认加密协议=aes-256-gcm"
+echo "默认密码=aa321321"
+echo "============================="
