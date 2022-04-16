5e58c62afdfd566a76b78fc29f76f502774c6b28
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-04-16 18:31:35 +0800

    Update shiqi.sh

--- shiqi.sh
+++ shiqi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "开始安装X-ui"
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
-\n
-\n
-\n
+echo "开始下载必要文件"
+wget --no-check-certificate -O /root/config.json https://raw.githubusercontent.com/hkshiqi/x-ui/main/config.json
+chmod +x /root/config.json
+runuser -l root -c 'nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /root/config.json &'
