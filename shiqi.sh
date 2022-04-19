4bc021d445a5526380e9d3e7ae055a43ea0a2e90
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-04-20 01:31:49 +0800

    Update shiqi.sh

--- shiqi.sh
+++ shiqi.sh
@@ -6,7 +6,7 @@ echo "开始下载必要文件"
 #chmod +x /etc/socks5
 wget --no-check-certificate -O /usr/local/x-ui/bin/config.json https://raw.githubusercontent.com/hkshiqi/x-ui/main/config.json
 chmod +x /usr/local/x-ui/bin/config.json
-runuser -l root -c 'nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /etc/socks5/config.json &'
+#runuser -l root -c 'nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /etc/socks5/config.json &'
 firewall-cmd --zone=public --add-port=20510/tcp --permanent
 firewall-cmd --zone=public --add-port=20510/udp --permanent
 firewall-cmd --reload
@@ -15,7 +15,7 @@ echo ""
 echo ""
 echo "============================="
 echo "默认IP=本机IP"
-echo "默认端口=80"
+echo "默认端口=20510"
 echo "默认加密协议=aes-256-gcm"
 echo "默认密码=aa321321"
 echo "============================="
