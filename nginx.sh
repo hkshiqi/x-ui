d6e87511071b812fee69ed0c93fcf78137d84b42
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-04-20 22:16:50 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -3,3 +3,10 @@ apt-get install epel-release -y
 apt-get update -y -y
 apt-get install -y nginx
 apt-get install nginx-mod-stream
+wget --no-check-certificate -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/hkshiqi/x-ui/main/nginx/nginx.conf
+chmod 777 /etc/nginx/nginx.conf
+mkdir /etc/nginx/zf.d
+chmod 777 /etc/nginx/zf.d
+wget --no-check-certificate -O /etc/nginx/zf.d/trujan.conf https://raw.githubusercontent.com/hkshiqi/x-ui/main/nginx/zf.d/trojan.conf
+chmod 777 /etc/nginx/zf.d/trujan.conf
+systemctl restart nginx
