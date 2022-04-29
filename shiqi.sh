76f8cee9279425c0de34863e8361d51eda2f6b42
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-04-29 08:05:18 +0800

    Update shiqi.sh

--- shiqi.sh
+++ shiqi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#系统性能优化
 echo 500000 >/proc/sys/kernel/pid_max
 echo 500000 >/proc/sys/kernel/threads-max
 echo 500000 >/proc/sys/vm/max_map_count
@@ -9,6 +10,7 @@ modprobe ip_conntrack
 echo 1000000 >/proc/sys/net/nf_conntrack_max
 lsmod |grep conntrack
 
+#安装nginx
 apt-get install epel-release -y
 apt-get update -y -y
 apt-get install -y nginx
@@ -20,6 +22,8 @@ chmod 777 /etc/nginx/zf.d
 wget --no-check-certificate -O /etc/nginx/zf.d/trujan.conf https://raw.githubusercontent.com/hkshiqi/x-ui/main/nginx/zf.d/trojan.conf
 chmod 777 /etc/nginx/zf.d/trujan.conf
 systemctl restart nginx
+
+#安装X-UI
 echo "开始安装X-ui"
 bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
 echo "开始下载必要文件"
@@ -30,18 +34,21 @@ wget --no-check-certificate -O /etc/socks5/qd.sh https://raw.githubusercontent.c
 wget --no-check-certificate -O /etc/socks5/config.json https://raw.githubusercontent.com/hkshiqi/x-ui/main/config.json
 chmod +x /etc/socks5/qd.sh
 chmod +x /etc/socks5/config.json
-#echo /etc/socks5/qd.sh >> /etc/rc.local
-#echo 'exit 0' >> /etc/rc.local
-#echo "runuser -l root -c 'nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /etc/socks5/config.json &'" >> /etc/rc.local
 wget --no-check-certificate -O /lib/systemd/system/qd.service https://raw.githubusercontent.com/hkshiqi/x-ui/main/qd.service
 sudo systemctl daemon-reload
 systemctl enable qd.service
 cd /etc/socks5
 ./qd.sh
+
+#防火墙放行端口
 firewall-cmd --zone=public --add-port=20510/tcp --permanent
 firewall-cmd --zone=public --add-port=20510/udp --permanent
 firewall-cmd --reload
+
+#安装启动BBR
 wget --no-check-certificate -O /etc/socks5/bbr.sh https://raw.githubusercontent.com/hkshiqi/x-ui/main/bbr && chmod +x /etc/socks5/bbr.sh && ./bbr.sh
+
+#shadowsocks信息输出
 echo ""
 echo ""
 echo ""
